92835e38a3c0c36a2890eb58947b90a28e9cac0c
Arkru <parkkr1204@naver.com>
2023-07-24 09:48:57 +0900

    [Feat] 플레이어 기본 조작
    * 사용자 입력 값 저장
    * 좌우 이동, 점프, 대시, 바닥 체크 구현
    * InputManager에 'Dash' 추가
    
    대시는 Vector2 입력을 받아 8방향 이동 가능하고, gravityScale = 0으로 만들어서 직선 이동이 가능함.

--- BB_Unity/Assets/Scripts/Player/PlayerController.cs
+++ BB_Unity/Assets/Scripts/Player/PlayerController.cs
@@ -9,21 +9,32 @@ public class PlayerController : MonoBehaviour
     private SpriteRenderer spriteRenderer;
     private PlayerMain playerMain;
 
-    private Vector2 boxCastSize = new Vector2(0.6f, 0.05f);
-    private float boxCastMaxDistance = 1.0f;
-
-    // ---------입력값 저장 변수---------
+    // 입력 값 저장 변수
     private Vector2 moveInput;
     private bool jumpInput;
     private bool dashInput;
     private bool attackInput;
 
-    // -------------인스펙터-------------
+    // 동작 확인 변수
+    private bool isMove = false;
+    private bool isJump = false;
+    private bool isDash = false;
+    private bool isAttack = false;
+
+    // 박스레이 조절
+    private Vector2 boxCastSize = new Vector2(0.6f, 0.05f);
+    private float boxCastMaxDistance = 1.0f;
+
+    // 인스펙터
     [Header("설정")]
     [SerializeField, Range(0f, 10f)]
-    private float maxSpeed = 5f;
+    private float moveSpeed = 5f;
     [SerializeField, Range(0f, 100f)]
     private float jumpPower = 20f;
+    [SerializeField, Range(0f, 100f)]
+    private float dashPower = 20f;
+    [SerializeField, Range(0f, 1f)]
+    private float dashSec = 0.2f;
 
     void Awake()
     {
@@ -33,102 +44,110 @@ public class PlayerController : MonoBehaviour
         playerMain = GetComponent<PlayerMain>();
     }
 
-    // 입력 함수
     void Update()
     {
-        //GetInput();
-    }
+        // 입력 값 저장
+        GetInput();
 
-    // 물리 함수
-    void FixedUpdate()
-    {
+        // 플레이어 조작
         Move();
         Jump();
+        Dash();
         Attack();
+
+        // 바닥 체크
         GroundCheck();
     }
 
     private void GetInput()
     {
-
+        moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        jumpInput = Input.GetButtonDown("Jump");
+        dashInput = Input.GetButtonDown("Dash");
+        attackInput = Input.GetButtonDown("Fire1");
     }
 
     private void Move()
     {
-        if (playerMain.IsHit())
+        if (isDash/*playerMain.IsHit()*/)
             return;
 
-        // 움직임 조작
-        float h = Input.GetAxisRaw("Horizontal"); // 횡으로 키를 누르면
-        rigid.AddForce(Vector2.right * h, ForceMode2D.Impulse); // 이동한다
+        //isMove = moveInput.magnitude != 0;
+        isMove = moveInput.x != 0;
 
-        // 최고 속도
-        if (rigid.velocity.x > maxSpeed) // 오른족
-            rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y);
-        else if (rigid.velocity.x < maxSpeed * (-1)) // 왼쪽
-            rigid.velocity = new Vector2(maxSpeed * (-1), rigid.velocity.y);
+        if (isMove)
+        {
+            // 움직임 조작
+            rigid.velocity = new Vector2(moveInput.x * moveSpeed, rigid.velocity.y);
 
-        // 움직임 멈춤
-        if (Input.GetButtonUp("Horizontal") || Input.GetAxisRaw("Horizontal") == 0) // 바로 멈춤
-            rigid.velocity = new Vector2(rigid.velocity.normalized.x * 0.0000001f, rigid.velocity.y);
+            // 방향 전환
+            //spriteRenderer.flipX = Input.GetAxisRaw("Horizontal") == -1;
+            transform.localScale = new Vector3(moveInput.x, transform.localScale.y, transform.localScale.z);
+        }
+        else
+            rigid.velocity = new Vector2(0f, rigid.velocity.y); // 미끄러짐 방지
 
-        // 방향 전환
-        //if (Input.GetButton("Horizontal"))
-        //    spriteRenderer.flipX = Input.GetAxisRaw("Horizontal") == -1;
-        if (Input.GetButton("Horizontal") && Input.GetAxisRaw("Horizontal") != 0) // 좌우키 동시 입력 버그 방지
-            transform.localScale = new Vector3(Input.GetAxisRaw("Horizontal"), transform.localScale.y, transform.localScale.z);
-
-        // 미끄러짐 방지
-        if (!(Input.GetButton("Horizontal")))
-            rigid.velocity = new Vector2(0, rigid.velocity.y);
-
-        //// 달리기 애니메이션
-        //if (Mathf.Abs(rigid.velocity.x) < 0.3) // 절댓값x가 크면 달리기
-        //    anim.SetBool("isRunning", false);
-        //else
-        //    anim.SetBool("isRunning", true); 
+        //anim.SetBool("isRunning", isMove);
     }
 
     private void Jump()
     {
-        if (playerMain.IsHit())
+        if (!jumpInput || isJump || isDash || isAttack/* || playerMain.IsHit()*/)
             return;
         
-        // 점프 (점프를 누르고, 점프 중이 아니고, 피격 상태가 아니면)
-        if (Input.GetButtonDown("Jump")/* && !anim.GetBool("isJumping")*/)
-        {
-            Debug.Log("jump!");
-            //rigid.velocity = new Vector2(rigid.velocity.x, 0); // 점프 속도 초기화
-            rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
-            //anim.SetBool("isJumping", true);
-        }
+        isJump = true;
+
+        //rigid.velocity = new Vector2(rigid.velocity.x, 0); // Y축 속도 초기화
+        rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+        //anim.SetBool("isJumping", true);
     }
 
     private void GroundCheck()
     {
-        if (playerMain.IsHit())
+        if (rigid.velocity.y > 0) // 추락이 아닐 때
             return;
 
-        // 바닥 판정
         //anim.SetBool("isFalling", true); // 점프 없이 낙하
 
-        //Debug.DrawRay(rigid.position, Vector3.down*2, new Color(0, 1, 0)); // 레이 시각화
-        //RaycastHit2D rayHit = Physics2D.Raycast(rigid.position, Vector3.down, 1, LayerMask.GetMask("Map"));
+        // 바닥 판정
         RaycastHit2D rayHit = Physics2D.BoxCast(transform.position, boxCastSize, 0f, Vector2.down, boxCastMaxDistance, LayerMask.GetMask("Ground"));
         if (rayHit.collider != null) // 바닥 감지를 위해서 레이저
         {
             if (rayHit.distance < 0.9f)
             {
+                isJump = false;
+
                 //anim.SetBool("isJumping", false);
                 //anim.SetBool("isFalling", false);
             }
         }
     }
 
+    private void Dash()
+    {
+        if (!dashInput || !isMove || isDash || isAttack/* || playerMain.IsHit()*/)
+            return;
+
+        isDash = true;
+        Debug.Log("dash!");
+        rigid.gravityScale = 0f; // 포물선 방지
+        rigid.velocity = new Vector2(0f, 0f); // 속도 초기화
+        rigid.AddForce(moveInput * dashPower, ForceMode2D.Impulse);
+
+        StartCoroutine(DashOut(dashSec));
+    }
+
+    IEnumerator DashOut(float second)
+    {
+        yield return new WaitForSeconds(second);
+        isDash = false;
+        rigid.gravityScale = 1f;
+    }
+
     private void Attack()
     {
-        if (playerMain.IsHit())
-            return;
+        //if (playerMain.IsHit())
+        //    return;
 
         // 공격 (현재 실행 중인 애니메이션이 공격이 아니면)
         if (Input.GetButtonDown("Fire1")/* && !anim.GetCurrentAnimatorStateInfo(0).IsName("Player1_AttackFront")*/)
